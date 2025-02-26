6de4f99d3f496473e384790a24e5cc4849515aaf
Owen Ziegler <odz2@txstate.edu>
2025-02-26 12:01:35 -0600

    Added main.ino
    Created main.ino, main driver file

---
+++ main/main.ino
@@ -0,0 +1,17 @@
+/*
+main.ino
+
+Owen Ziegler
+2025 02 26
+
+Description:
+Main driver file for Trace Invaders robot code. Contains the main loop & brings module files together.
+*/
+
+void setup() {
+  //setup goes here
+}
+
+void loop() {
+  //state machine goes here
+}
